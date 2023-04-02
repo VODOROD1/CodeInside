ff6d1508221048fbfb78e59f34f209d9cc1e84a9
Mike_Belyakov <48880903+VODOROD1@users.noreply.github.com>
2023-04-02 14:27:23 +0400

    FilterComponent was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParametersComponent } from './components/parameters/parameters.component';
 import { TypeComponent } from './components/type/type.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParametersComponent,
-    TypeComponent
+    TypeComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
